24c3642754c619971b7d2c63adfa3b17675f5d9c
Togene <jhonnywhiplash@hotmail.com.au>
2017-03-07 01:17:16 +1000

    Update : Added Shader Changes

---
+++ Passage_Main/Assets/Atmosphere_Manager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Atmosphere_Manager : MonoBehaviour
+{
+    [Range(0.0f , -0.1f)]
+    public float KM_Range = -.0015f;
+
+    [Range(0.000f, 0.002f)]
+    public float KR_Range = .0015f;
+
+    Material skyMat;
+
+    void Start ()
+    {
+        skyMat = GetComponent<MeshRenderer>().material;
+    }
+
+    void Update()
+    {
+        skyMat.SetFloat("_Km", KM_Range);
+        skyMat.SetFloat("_Kr", KR_Range);
+    }
+
+}
